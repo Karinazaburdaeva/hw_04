2b9dae9213f5e0fd5dffd5732cc0991c4fff0f3e
karina <karinazaburdaeva2222@gmail.com>
2023-09-25 21:32:37 +0300

    calendar

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -6,4 +6,19 @@
      Unknown = -1,
      ConnectionLost = 200
     }
+
+
+    public enum Days
+    {
+        None = 0,
+        Monday = 0b_0000_0001, // 1,
+        Tuesday = 0b_0000_0010, // 1,
+        Wednesday = 0b_0000_0100, // 1,
+        Thursday = 0b_0001_0000, // 1,
+        Friday = 0b_0010_0000, // 1,
+        Saturday = 0b_0000_0001, // 1,
+        Sunday = 0b_0100_0000 // 1,
+        
+
+    }
 }
--- hw_04/Program.cs
+++ hw_04/Program.cs
@@ -6,7 +6,21 @@
         {
             Model.ErrorCode myVariable = Model.ErrorCode.ConnectionLost;
             Console.WriteLine("MyVariable = {0}", myVariable);
- 
+
+            var today = Model.Days.Sunday;
+            int todayInt = (int)today;
+            Console.WriteLine("Today variable = {0}", today);
+            Console.WriteLine("TodayInt variable = {0}", todayInt);
+
+            int tommorowInt = 2;
+            Model.Days tommorowEnum = (Model.Days)tommorowInt;
+            Console.WriteLine("tommorowEnum variable = {0}", tommorowEnum);
+
+            int intNine = 9;
+            Model.Days nineEnum = (Model.Days)intNine;
+            Console.WriteLine("nineEnum variable = {0}", nineEnum);
+
+
         }
     }
 }
