c7618ba50be31211382b7cd87fef0c287722faee
karina <karinazaburdaeva2222@gmail.com>
2023-09-28 21:47:32 +0300

    new

--- hw_04/Program.cs
+++ hw_04/Program.cs
@@ -1,18 +1,18 @@
 ﻿namespace Host
-using Model
+
 {
     internal class Program
     {
         static void Main()
         {
            double square = 200;
-           for (int year = 1, year <=8, year++)
+            for (int year = 1, year <= 8, year++);
         {
             square *= 1.1;
         }
-        {
+        
             Console.WriteLine("Итоговая площадь участка:" + year);
-        }
+        
 
         }
     }
